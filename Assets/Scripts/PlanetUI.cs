b3489afa31bd3fb848f8d1f2e4c78702c62399f8
dafors <da_fors@hotmail.com>
2020-11-12 10:32:23 +0100

    Add clickable planet prefab. Can be set to resize on click.

---
+++ Assets/Scripts/PlanetUI.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Image = UnityEngine.UI.Image;
+
+[RequireComponent(typeof(Image))]
+public class PlanetUI : MonoBehaviour, IPointerClickHandler
+{
+    [Tooltip("multiplayer to enlarge planet image on click")] public float enlargeMultiplier;
+    private Vector3 sizeMultiplier = Vector3.one;
+    private Image image;
+    
+    private void Start()
+    {
+        this.image = GetComponent<Image>();
+        if (this.image == null)
+        {
+            Debug.Log("Warning, no image found", this);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            ClickEvent();
+        }
+    }
+
+    private void ClickEvent()
+    {
+        this.image.transform.localScale = sizeMultiplier;
+        //TODO create popup or switch screen.
+        
+        // If changing scene
+        // LoadScene(string sceneName, SceneManagement.LoadSceneMode mode = LoadSceneMode.Single);
+        
+        // If popup
+        // Instantiate();
+    }
+    
+    void OnValidate()
+    {
+        this.sizeMultiplier = Vector3.one * enlargeMultiplier;
+    }
+}
