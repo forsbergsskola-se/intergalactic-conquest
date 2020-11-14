1ecf4772eaa66fbf06991d94e1f91cb1965c82df
Linus Vallinder <divinator.dev@gmail.com>
2020-11-14 21:16:36 +0100

    Worked on Planet UI

--- Assets/Scripts/Strategies/Strategy.cs
+++ Assets/Scripts/Strategies/Strategy.cs
@@ -5,4 +5,18 @@ public class Strategy : ScriptableObject
 {
     [Header("Sub-Strategies")]
     public SubStrategy[] SubStrategies;
+
+    public int Level{ get{ return GetStrategyLevel(); } private set{} }
+
+    public int GetStrategyLevel(){
+
+        int level = 0;
+
+        foreach (var strategy in SubStrategies){
+            
+            Level += strategy.Level;
+        }
+
+        return level / 2;
+    }
 }
