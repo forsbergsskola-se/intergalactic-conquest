b7834eaa5d382cccc7caf7fb3788525c54ad8e88
Virginia Baroni <virg.baroni@gmail.com>
2020-11-17 16:53:03 +0100

    moved volume settings into options to be single menu, added art

--- Assets/Scripts/Menu/MenuSettings.cs
+++ Assets/Scripts/Menu/MenuSettings.cs
@@ -7,9 +7,6 @@ using UnityEngine.Audio;
 [RequireComponent (typeof(Slider))]
 public class MenuSettings : MonoBehaviour
 {
-    private Resolution[] resolutions;
-    public Dropdown resolutionDropdown;
-    
     Slider slider
     {
         get { return GetComponent<Slider>(); }
@@ -20,34 +17,11 @@ public class MenuSettings : MonoBehaviour
 
     private void Start()
     {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-        List<string> options = new List<string>();
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
-            options.Add(option);
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
         ResetSliderValue();
         slider.onValueChanged.AddListener(delegate
         {
             UpdateValueOnChange(slider.value);
         });
-    }
-
-    public void SetResolution(int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
     public void SetQuality(int qualityIndex)
     {
---
+++ Assets/Scripts/Menu/ResolutionSettings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class ResolutionSettings : MonoBehaviour
+{
+    private Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+    
+    private void Start()
+    {
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
